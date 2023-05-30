04a429447925708a5106472d495360be9c7370ff
orestbrowkovn <135020376+orestbrowkovn@users.noreply.github.com>
2023-05-30 14:16:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,72 @@
+// Поліровані
+var photoBlock = document.getElementById("polished");
+var imageBlock = document.querySelector(".color-about-center img");
 
+photoBlock.onclick = function() {
+  imageBlock.style.opacity = 0; // сначала делаем картинку прозрачной
+  imageBlock.src = "https://mario-radiators.com/wp-content/uploads/2023/03/bez-imeni-2.png";
+  setTimeout(function() {
+    imageBlock.style.opacity = 1; // плавно делаем картинку непрозрачной
+  }, 50);
+};
+
+
+// Сатин
+var photoBlock = document.getElementById("brushed");
+var imageBlock = document.querySelector(".color-about-center img");
+
+photoBlock.onclick = function() {
+  imageBlock.style.opacity = 0; // сначала делаем картинку прозрачной
+  imageBlock.src = "https://mario-radiators.com/wp-content/uploads/2023/03/modena-800x600-brushed.png";
+  setTimeout(function() {
+    imageBlock.style.opacity = 1; // плавно делаем картинку непрозрачной
+  }, 50);
+};
+
+// білий глянець
+var photoBlock = document.getElementById("white-gloss");
+var imageBlock = document.querySelector(".color-about-center img");
+
+photoBlock.onclick = function() {
+	imageBlock.style.opacity = 0; // сначала делаем картинку прозрачной
+  imageBlock.src = "https://mario-radiators.com/wp-content/uploads/2023/03/modena-800x600-white.png";
+  setTimeout(function() {
+    imageBlock.style.opacity = 1; // плавно делаем картинку непрозрачной
+  }, 50);
+};
+
+// чорний мат
+var photoBlock = document.getElementById("black-mat");
+var imageBlock = document.querySelector(".color-about-center img");
+
+photoBlock.onclick = function() {
+	imageBlock.style.opacity = 0; // сначала делаем картинку прозрачной
+  imageBlock.src = "https://mario-radiators.com/wp-content/uploads/2023/03/modena-800x600-black-mat.png";
+  setTimeout(function() {
+    imageBlock.style.opacity = 1; // плавно делаем картинку непрозрачной
+  }, 50);
+};
+
+// Бронозовий
+var photoBlock = document.getElementById("bronze");
+var imageBlock = document.querySelector(".color-about-center img");
+
+photoBlock.onclick = function() {
+	imageBlock.style.opacity = 0; // сначала делаем картинку прозрачной
+  imageBlock.src = "https://mario-radiators.com/wp-content/uploads/2023/03/modena-800x600-bronze.png";
+  setTimeout(function() {
+    imageBlock.style.opacity = 1; // плавно делаем картинку непрозрачной
+  }, 50);
+};
+
+// Золото
+var photoBlock = document.getElementById("gold");
+var imageBlock = document.querySelector(".color-about-center img");
+
+photoBlock.onclick = function() {
+	imageBlock.style.opacity = 0; // сначала делаем картинку прозрачной
+  imageBlock.src = "https://mario-radiators.com/wp-content/uploads/2023/03/modena-800x600-gold.png";
+  setTimeout(function() {
+    imageBlock.style.opacity = 1; // плавно делаем картинку непрозрачной
+  }, 50);
+};
